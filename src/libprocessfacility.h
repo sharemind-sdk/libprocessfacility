06cf8231ffa09a46a69770054ceb4102bce3a5b9
Karl Tarbe <karl.tarbe@cyber.ee>
2015-11-04 22:28:34 +0200

    This was extracted from appserver/src/Facilities

---
+++ src/libprocessfacility.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_MINER_PROCESSFACILITYAPI_H
+#define SHAREMIND_MINER_PROCESSFACILITYAPI_H
+
+#ifdef __cplusplus
+#include <cstdint>
+#endif
+#include <sharemind/libmodapi/api_0x1.h>
+#ifndef __cplusplus
+#include <stdint.h>
+#endif
+
+
+#ifndef SHAREMIND_ICONST
+#ifdef SHAREMIND_INTERNAL__
+#define SHAREMIND_ICONST
+#else
+#define SHAREMIND_ICONST const
+#endif
+#endif /* SHAREMIND_ICONST */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint16_t SharemindProcessId;
+
+struct SharemindProcessFacility_;
+typedef struct SharemindProcessFacility_ SharemindProcessFacility;
+struct SharemindProcessFacility_ {
+    SharemindProcessId (* SHAREMIND_ICONST get_process_id)(
+            const SharemindProcessFacility * facility);
+};
+
+#ifdef __cplusplus
+} /* extern "C" { */
+#endif
+
+#endif /* SHAREMIND_MINER_PROCESSFACILITYAPI_H */
